5bb7ec37013fd5c10c240301689d4d467c0b2a87
Matvey Nekrasov Home <metal59@gmail.com>
2022-12-09 04:18:54 +0700

    добавлен модуль "Сортировка"

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import FilterView from './view/filter-view.js';
+import SortView from './view/sort-view.js';
 import { render } from './render.js';
 
 const filterContainerElement = document.querySelector('.trip-controls__filters');
+const mainContentElement = document.querySelector('.trip-events');
 
 render(new FilterView(), filterContainerElement);
+render(new SortView(), mainContentElement);
--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -2,7 +2,8 @@ import { createElement } from '../render.js';
 
 function createFilterTemplate() {
   return (
-    `<form class="trip-filters" action="#" method="get">
+    `
+    <form class="trip-filters" action="#" method="get">
       <div class="trip-filters__filter">
         <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything">
         <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
@@ -14,7 +15,8 @@ function createFilterTemplate() {
       </div>
 
       <button class="visually-hidden" type="submit">Accept filter</button>
-    </form>`
+    </form>
+    `
   );
 }
 
