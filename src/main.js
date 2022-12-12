c8f4efc83b8dc6708dec6bd46b868634352851a4
Matvey Nekrasov Home <metal59@gmail.com>
2022-12-12 07:27:56 +0700

    подключена модель

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import FilterView from './view/filter-view.js';
 import { render } from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
+import PointsModel from './model/points-model.js';
 
 const filterContainerElement = document.querySelector('.trip-controls__filters');
 const mainContentElement = document.querySelector('.trip-events');
-const boardPresenter = new BoardPresenter({ boardContainer: mainContentElement });
+const pointsModel = new PointsModel();
+const boardPresenter = new BoardPresenter({
+  boardContainer: mainContentElement,
+  pointsModel,
+});
 
 render(new FilterView(), filterContainerElement);
 boardPresenter.init();
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -7,16 +7,19 @@ import { render } from '../render.js';
 export default class BoardPresenter {
   tripListComponent = new TripListView();
 
-  constructor({ boardContainer }) {
+  constructor({ boardContainer, pointsModel }) {
     this.boardContainer = boardContainer;
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    this.boardPoints = [...this.pointsModel.getPoints()];
+
     render(new SortView(), this.boardContainer);
     render(this.tripListComponent, this.boardContainer);
     render(new PointEditView(), this.tripListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < this.boardPoints.length; i++) {
       render(new PointView(), this.tripListComponent.getElement());
     }
   }
