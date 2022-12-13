0e7b47fb59869407654af30d6f979e4d8427662e
Matvey Nekrasov Home <metal59@gmail.com>
2022-12-13 21:16:44 +0700

    добавлено динамическое заполнение point-edit-view

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,5 +1,6 @@
 import SortView from '../view/sort-view.js';
 import TripListView from '../view/trip-list-view.js';
+import PointEditView from '../view/point-edit-view.js';
 import PointView from '../view/point-view.js';
 import { render } from '../render.js';
 
@@ -24,6 +25,8 @@ export default class BoardPresenter {
   init() {
     render(new SortView(), this.boardContainer);
     render(this.tripListComponent, this.boardContainer);
+    render(new PointEditView(this.getPointViewData()), this.tripListComponent.getElement());
+    render(new PointEditView(this.getPointViewData(this.points[0])), this.tripListComponent.getElement());
     for (let i = 1; i < this.points.length; i++) {
       render(new PointView(this.getPointViewData(this.points[i])), this.tripListComponent.getElement());
     }
