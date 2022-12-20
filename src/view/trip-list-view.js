7d82d127533258cd6fd0ef3e435f58966570aa89
Matvey Nekrasov <metal59@gmail.com>
2022-12-21 05:41:01 +0700

    добавлено наследование простым компонентам

--- src/view/trip-list-view.js
+++ src/view/trip-list-view.js
@@ -1,25 +1,11 @@
-import { createElement } from '../render.js';
+import AbstractView from '../framework/view/abstract-view.js';
 
 function createTripListTemplate() {
   return '<ul class="trip-events__list"></ul>';
 }
 
-export default class TripListView {
-  #element = null;
-
+export default class TripListView extends AbstractView {
   get template() {
     return createTripListTemplate();
-  }
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
-  removeElement() {
-    this.#element = null;
   }
 }
