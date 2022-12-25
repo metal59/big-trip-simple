1c139bc3c6745a9d211cb27cbdd53ef5d2d62a39
metal <metal59@gmail.com>
2022-12-25 12:56:26 +0000

    подключены моки фильтров

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { render } from './framework/render.js';
 import FilterView from './view/filter-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import PointsModel from './model/points-model.js';
+import { generateFilter } from './mock/filter.js';
 
 const filterContainerElement = document.querySelector('.trip-controls__filters');
 const mainContentElement = document.querySelector('.trip-events');
@@ -11,5 +12,7 @@ const boardPresenter = new BoardPresenter({
   pointsModel,
 });
 
-render(new FilterView(), filterContainerElement);
+const filters = generateFilter(pointsModel.points);
+
+render(new FilterView({ filters }), filterContainerElement);
 boardPresenter.init();
