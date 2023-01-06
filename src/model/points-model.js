b91c501f97a39bc98fb2723fb0d5b44f21e5050b
Matvey Nekrasov <metal59@gmail.com>
2023-01-06 19:25:42 +0700

    использован паттерн наблюдатель

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,9 +1,10 @@
+import Observable from '../framework/observable.js';
 import { getPoints } from '../mock/point.js';
 import { getOffers } from '../mock/offer.js';
 import { getOffersByType } from '../mock/offers-by-type.js';
 import { getDestinations } from '../mock/destination.js';
 
-export default class PointsModel {
+export default class PointsModel extends Observable{
   #points = [...getPoints()];
   #allOffers = getOffers();
   #offersByType = getOffersByType();
