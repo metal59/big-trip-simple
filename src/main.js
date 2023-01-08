ca344e4503e412d4152aba77ef1342b5da77f7bc
Matvey Nekrasov <metal59@gmail.com>
2023-01-09 04:15:09 +0700

    доавблена модель для фильтра

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import FilterView from './view/filter-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import PointsModel from './model/points-model.js';
 import PointCommonModel from './model/point-common-model.js';
+import FilterModel from './model/filter-model.js';
 import { generateFilter } from './mock/filter.js';
 
 const filterContainerElement = document.querySelector('.trip-controls__filters');
 const mainContentElement = document.querySelector('.trip-events');
 const pointsModel = new PointsModel();
 const pointCommonModel = new PointCommonModel();
+const filterModel = new FilterModel();
 const boardPresenter = new BoardPresenter({
   boardContainer: mainContentElement,
   pointsModel,
