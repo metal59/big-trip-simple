5bf5502737b32961c4641e5a89570cdb30a86b26
Matvey Nekrasov W <metal59@gmail.com>
2023-01-09 14:48:50 +0000

    добавлен модуль «Кнопка создания новой точки»

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
+import {render} from './framework/render.js';
+import NewPointButtonView from './view/new-point-button-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import PointsModel from './model/points-model.js';
 import PointCommonModel from './model/point-common-model.js';
 import FilterModel from './model/filter-model.js';
 
-const filterContainerElement = document.querySelector('.trip-controls__filters');
 const mainContentElement = document.querySelector('.trip-events');
+const filterContainerElement = document.querySelector('.trip-controls__filters');
+const siteHeaderElement = document.querySelector('.trip-main');
 const pointsModel = new PointsModel();
 const pointCommonModel = new PointCommonModel();
 const filterModel = new FilterModel();
@@ -20,6 +23,8 @@ const filterPresenter = new FilterPresenter({
   filterModel,
   pointsModel
 });
+
+render(new NewPointButtonView(), siteHeaderElement);
 
 filterPresenter.init();
 boardPresenter.init();
