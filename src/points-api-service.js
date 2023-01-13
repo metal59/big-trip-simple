deabe1aff8ae2a65fbc4657d27f6bf22661cdd0f
Matvey Nekrasov <metal59@gmail.com>
2023-01-13 19:16:16 +0700

    "включено" обновление задач на сервере

--- src/points-api-service.js
+++ src/points-api-service.js
@@ -15,12 +15,31 @@ export default class PointsApiService extends ApiService {
     const response = await this._load({
       url: `points/${point.id}`,
       method: Method.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({ 'Content-Type': 'application/json' }),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  #adaptToServer(point) {
+    const adaptedPoint = {
+      ...point,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null,
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null,
+      'destination': point.destId,
+      'offers': point.selectedOffers,
+    };
+
+    delete adaptedPoint['basePrice'];
+    delete adaptedPoint['dateFrom'];
+    delete adaptedPoint['dateTo'];
+    delete adaptedPoint['destId'];
+    delete adaptedPoint['selectedOffers'];
+
+    return adaptedPoint;
   }
 }
