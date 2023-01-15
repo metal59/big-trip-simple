3ca16d2c1e58ef64d8a2578542a01ee7b86bd46b
Matvey Nekrasov <metal59@gmail.com>
2023-01-15 19:16:59 +0700

    добавлена поддержка обратной связи при ошибке

--- src/presenter/new-point-presenter.js
+++ src/presenter/new-point-presenter.js
@@ -54,6 +54,18 @@ export default class NewPointPresenter {
     });
   }
 
+  setAborting() {
+    const resetFormState = () => {
+      this.#pointEditComponent.updateElement({
+        isDisabled: false,
+        isSaving: false,
+        isDeleting: false,
+      });
+    };
+
+    this.#pointEditComponent.shake(resetFormState);
+  }
+
   #handleFormSubmit = (point) => {
     this.#handleDataChange(
       UserAction.ADD_POINT,
--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -96,6 +96,23 @@ export default class PointPresenter {
     }
   }
 
+  setAborting() {
+    if (this.#mode === Mode.DEFAULT) {
+      this.#pointComponent.shake();
+      return;
+    }
+
+    const resetFormState = () => {
+      this.#pointEditComponent.updateElement({
+        isDisabled: false,
+        isSaving: false,
+        isDeleting: false,
+      });
+    };
+
+    this.#pointEditComponent.shake(resetFormState);
+  }
+
   #replaceEventToForm() {
     replace(this.#pointEditComponent, this.#pointComponent);
     document.addEventListener('keydown', this.#escKeyDownHandler);
